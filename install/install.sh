662b81d38f97c7446ccd41791221250ffc8baf9e
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-02-11 23:47:38 +0700

    Update installer script

--- install/install.sh
+++ install/install.sh
@@ -11,9 +11,10 @@ usage() {
    cat << EOT
 Usage: $0 [option] command
 Options:
-   --node       if a new node.sh file should be downloaded from harmony-one/harmony (master)
-   --s3-url     what s3 base url to use for uploading binaries (defaults to s3://tools.harmony.one/release/linux-x86_64/harmony)
-   --help       print this help section
+   --node                   if a new node.sh file should be downloaded from harmony-one/harmony (master)
+   --node-sh-url    path    where to download node.sh from (defaults to https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh)
+   --binaries-url   path    where to download the binaries from (defaults to http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony)
+   --help                   print this help section
 EOT
 }
 
@@ -21,7 +22,8 @@ while [ $# -gt 0 ]
 do
   case $1 in
   --node) should_download_node_sh=true ;;
-  --s3-url) s3_url="$2" ; shift;;
+  --node-sh-url) node_sh_url="$2" ; shift;;
+  --binaries-url) binaries_url="$2" ; shift;;
   -h|--help) usage; exit 1;;
   (--) shift; break;;
   (-*) usage; exit 1;;
@@ -37,22 +39,26 @@ initialize() {
     should_download_node_sh=false
   fi
 
-  if [ -z "$s3_url" ]; then
-    s3_url="s3://tools.harmony.one/release/linux-x86_64/harmony"
+  if [ -z "$node_sh_url" ]; then
+    node_sh_url="https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh"
+  fi
+
+  if [ -z "$binaries_url" ]; then
+    binaries_url="http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony"
   fi
 }
 
 download_binaries() {
   for binary in "${binaries[@]}"; do
-    echo "Downloading ${binary} from ${s3_url}/${binary} ..."
-    rm -rf ${binary} && wget ${s3_url}/${binary} && chmod u+x ${binary}
+    echo "Downloading ${binary} from ${binaries_url}/${binary} ..."
+    rm -rf ${binary} && wget ${binaries_url}/${binary} && chmod u+x ${binary}
   done
 }
 
 download_node_sh() {
   echo ""
   echo "Downloading the latest node.sh from harmony-one/harmony (master) ..."
-  rm -rf node.sh && wget https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/node.sh && chmod u+x node.sh
+  rm -rf node.sh && wget ${node_sh_url} && chmod u+x node.sh
 }
 
 install() {
